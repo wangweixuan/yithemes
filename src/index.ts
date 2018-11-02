40a09c9fab9780152c921bb6ecdd4ca8f1d30128
Wang Weixuan <wangweixvan@gmail.com>
2018-11-02 18:02:43 +0800

    Refactor markdown preview theming

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import { writeFile } from 'fs'
+import * as tc from 'tinycolor2'
 import { dark as darkSyntax, light as lightSyntax} from './syntax'
 import { dark as darkWorkbench, light as lightWorkbench} from './workbench'
-import mdpreview from './mdpreview'
-import { interpret } from './utilities'
+import { markdownCSS } from './markdown'
+
+function interpret(key: string, value: any) {
+  if (value && value instanceof tc) {
+    const color = value as tc.Instance
+    return color.getAlpha() === 1 ? color.toHexString() : color.toHex8String()
+  }
+  return value
+}
 
 writeFile('dist/dark.json', JSON.stringify({
   $schema: 'vscode://schemas/color-theme',
@@ -20,4 +28,4 @@ writeFile('dist/light.json', JSON.stringify({
   tokenColors: lightSyntax
 }, interpret).replace(/_/g, '.'), err => { if (err) throw err })
 
-writeFile('dist/mdpreview.css', mdpreview, err => { if (err) throw err })
+writeFile('dist/markdown.css', markdownCSS, err => { if (err) throw err })
