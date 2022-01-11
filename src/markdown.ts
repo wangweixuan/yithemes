79d585abfdcc63773c6d6a182d203706221fb19b
Wang Weixuan <wangweixvan@gmail.com>
2022-01-11 16:57:18 +0800

    Adopt Dart Sass

--- src/markdown.ts
+++ src/markdown.ts
@@ -1,25 +1,31 @@
-import { renderSync, types } from 'node-sass'
 import { join } from 'path'
+import { compile, SassColor, Value as SassValue } from 'sass'
 import * as tc from 'tinycolor2'
 import * as darkPalette from './dark'
 import * as lightPalette from './light'
 
-function getColor<T>(palette: T, id: types.Value) {
-  if (!(id instanceof types.String)) throw new Error()
-  const realId = id.getValue() as keyof T
+function getColor<T>(palette: T, id: SassValue) {
+  const realId = id.assertString('id').text as keyof T
 
   const tcColor = palette[realId]
   if (!(tcColor instanceof tc)) throw new Error()
   const color = tcColor.toRgb()
 
-  return new types.Color(color.r, color.g, color.b, color.a)
+  return new SassColor({
+    red: color.r,
+    green: color.g,
+    blue: color.b,
+    alpha: color.a
+  })
 }
 
-export const MARKDOWN_CSS = renderSync({
-  file: join(__dirname, '../../src/markdown.scss'),
-  functions: {
-    'dark-palette($id)': (id: types.Value) => getColor(darkPalette, id),
-    'light-palette($id)': (id: types.Value) => getColor(lightPalette, id)
-  },
-  outputStyle: 'compressed'
-}).css
+export const MARKDOWN_CSS = compile(
+  join(__dirname, '../../src/markdown.scss'),
+  {
+    functions: {
+      'dark-palette($id)': ([id]) => getColor(darkPalette, id),
+      'light-palette($id)': ([id]) => getColor(lightPalette, id)
+    },
+    style: 'compressed'
+  }
+).css
